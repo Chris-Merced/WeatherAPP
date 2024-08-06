035b87c566975e805fc50c27c995098651cddf9c
Chris-Merced <148519061+Chris-Merced@users.noreply.github.com>
2024-08-06 08:21:24 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Template",
+            template: "./src/index.html"
+        })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        watchFiles: './src/index.html'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+     },
+};
